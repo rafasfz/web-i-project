5ddd90e80a09086a2dbde00812c3833e62974782
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2022-04-28 14:05:25 -0300

    adicionado botao de submit na tela de cadastro

--- bazartech-frontend/src/routes/sign-up/index.svelte
+++ bazartech-frontend/src/routes/sign-up/index.svelte
@@ -6,6 +6,7 @@
   const handleSubmit = () => {
     console.log(email, password, confirm_password);
     // Cadastrar na API
+    // Logar na API
     // Adicionar user no store de usuarios
     // Adicionar user e token no cookie o localstorage
     // Redirecionar para a home
@@ -18,9 +19,10 @@
 
 <div>
   <h1>Criar conta</h1>
-  <form action="">
+  <form action="" on:submit|preventDefault={handleSubmit}>
     <input type="text" name="email" id="email" placeholder="E-mail" bind:value={email} />
     <input type="password" name="password" placeholder="Senha" bind:value={password} />
     <input type="password" name="confirm_password" placeholder="Insira a senha novamente" bind:value={confirm_password} />
+    <input type="submit" name="submit" id="submit" />
   </form>
 </div>
